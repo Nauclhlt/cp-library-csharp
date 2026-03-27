dac74815ac46c4128a26cc34033297da610d0ecc
Nauclhlt <111179210+Nauclhlt@users.noreply.github.com>
2026-03-28 00:15:53 +0900

    initial commit

---
+++ library/data-structure/SegmentTree.cs
@@ -0,0 +1,154 @@
+/// <summary>
+/// Segment tree. (Non-recursive, compatible for non-commutative monoid)
+/// </summary>
+/// <typeparam name="T">Type of values.</typeparam>
+public sealed class SegmentTree<T> where T : struct
+{
+    public delegate T Monoid(T x, T y);
+
+    private int _treeSize;
+    private int _dataSize;
+    private int _originalDataSize;
+    private T[] _data;
+    private Monoid _operator;
+    private Monoid _update;
+    private T _identity;
+
+    public int OriginalDataSize => _originalDataSize;
+    public int TreeSize => _treeSize;
+    public T Identity => _identity;
+
+    /// <summary>
+    /// Gets the value by index. Time complexity is O(1).
+    /// </summary>
+    public T this[int index]
+    {
+        get
+        {
+            return _data[_dataSize - 1 + index];
+        }
+    }
+
+    /// <summary>
+    /// Builds the segment tree. Time complexity is O(n).
+    /// </summary>
+    public SegmentTree(int n, Monoid op, Monoid update, T identity)
+    {
+        _originalDataSize = n;
+
+        int size = 1;
+        while (n > size)
+        {
+            size <<= 1;
+        }
+
+        _dataSize = size;
+        _treeSize = 2 * size - 1;
+
+        _data = new T[_treeSize];
+        Array.Fill(_data, identity);
+        _identity = identity;
+        _operator = op;
+        _update = update;
+    }
+
+    /// <summary>
+    /// Rebuild the segment tree from the array. Time complexity is O(n).
+    /// Since the time complexity of n Update() calls is O(nlogn), when initializing the segment tree with an array, 
+    /// call this function to make it faster.
+    /// </summary>
+    public void Build(T[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            _data[i + _dataSize - 1] = array[i];
+        }
+
+        for (int i = _dataSize - 2; i >= 0; i--)
+        {
+            _data[i] = _operator(_data[(i << 1) + 1], _data[(i << 1) + 2]);
+        }
+    }
+
+    public void UpdateByArray(T[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            _data[i + _dataSize - 1] = _update(_data[i + _dataSize - 1], array[i]);
+        }
+
+        for (int i = _dataSize - 2; i >= 0; i--)
+        {
+            _data[i] = _operator(_data[(i << 1) + 1], _data[(i << 1) + 2]);
+        }
+    }
+
+    /// <summary>
+    /// Fill the array with the uniform value. Time complexity is O(n).
+    /// </summary>
+    public void Fill(T value)
+    {
+        for (int i = 0; i < _originalDataSize; i++)
+        {
+            _data[i + _dataSize - 1] = value;
+        }
+
+        for (int i = _dataSize - 2; i >= 0; i--)
+        {
+            _data[i] = _operator(_data[(i << 1) + 1], _data[(i << 1) + 2]);
+        }
+    }
+
+    /// <summary>
+    /// Update the value at the specified position. Time complexity is O(logn).
+    /// </summary>
+    public void Update(int index, T value)
+    {
+        index += _dataSize - 1;
+        _data[index] = _update(_data[index], value);
+
+        while (index > 0)
+        {
+            index = (index - 1) >> 1;
+            _data[index] = _operator(_data[(index << 1) + 1], _data[(index << 1) + 2]);
+        }
+    }
+
+    /// <summary>
+    /// Gets the product of the range [l, r). Time complexity is O(logn).
+    /// </summary>
+    public T Fold(int l, int r)
+    {
+        l += _dataSize - 1;
+        r += _dataSize - 1;
+
+        T leftFold = _identity;
+        T rightFold = _identity;
+        while (l < r)
+        {
+            if ((l & 1) == 0)
+            {
+                leftFold = _operator(leftFold, _data[l]);
+                l++;
+            }
+            if ((r & 1) == 0)
+            {
+                r--;
+                rightFold = _operator(_data[r], rightFold);
+            }
+
+            l = (l - 1) >> 1;
+            r = (r - 1) >> 1;
+        }
+
+        return _operator(leftFold, rightFold);
+    }
+
+    /// <summary>
+    /// Returns the span of the underlying data. Time complexity is O(1).
+    /// </summary>
+    public ReadOnlySpan<T> AsSpan()
+    {
+        return _data.AsSpan(_dataSize - 1, _originalDataSize);
+    }
+}
---
+++ verify/data-structure/SegmentTree.test.csx
@@ -0,0 +1,31 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/point_add_range_sum
+
+#load "..\..\library\data-structure\SegmentTree.cs"
+
+string[] nq = Console.ReadLine().Split();
+int N = int.Parse(nq[0]);
+int Q = int.Parse(nq[1]);
+
+var seg = new SegmentTree<long>(N, (x, y) => x + y, (x, y) => x + y, 0L);
+
+long[] arr = Console.ReadLine().Split().Select(x => long.Parse(x)).ToArray();
+seg.Build(arr);
+
+while (Q-- > 0)
+{
+    var q = Console.ReadLine().Split();
+    int t = int.Parse(q[0]);
+
+    if (t == 0)
+    {
+        int p = int.Parse(q[1]);
+        int x = int.Parse(q[2]);
+        seg.Update(p, x);
+    }
+    else
+    {
+        int l = int.Parse(q[1]);
+        int r = int.Parse(q[2]);
+        Console.WriteLine(seg.Fold(l, r));
+    }
+}
